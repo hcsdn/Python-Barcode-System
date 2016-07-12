47297f3139a2adc296a663fd6a9be88c7d1bae7e
Jameson_cn <3676526@qq.com>
2016-07-12 10:43:18 +0800

    add tables

--- BarcodeSystem/basic/models.py
+++ BarcodeSystem/basic/models.py
@@ -10,4 +10,23 @@ class barcode(models.Model):
     createdby = models.CharField(max_length=50)
     lastmodifieddate = models.DateTimeField()
     lastmodifiedby = models.CharField(max_length=50)
-    
+   
+class Region(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=200)
+    createddate = models.DateTimeField()
+    createdby = models.CharField(max_length=50)
+    lastmodifieddate = models.DateTimeField()
+    lastmodifiedby = models.CharField(max_length=50)
+
+
+class WorkShift(models.Model):
+    id= models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=200)
+    region = models.ForeignKey(Region)
+    createddate = models.DateTimeField()
+    createdby = models.CharField(max_length=50)
+    lastmodifieddate = models.DateTimeField()
+    lastmodifiedby = models.CharField(max_length=50)
